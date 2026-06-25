00e7acfc7b9a9971c09578064bfb0c8203c9ceb0
wanjugu-wq <wanjuguzzz@gmail.com>
2026-06-25 13:49:46 +0300

    Add GET endpoints for homepage and products

--- app.py
+++ app.py
@@ -7,19 +7,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    return {"message": "Welcome to the Flatiron Store!"} # TODO: Return a welcome message
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
-@app.route("/products")
+@app.route("/products", methods = ["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    category = request.args.get("category") # TODO: Return all products or filter by ?category=
+
+    if category:
+        filtered_products = [
+            product for product in products
+            if product['category'].lower == category.lower
+        ]
+        return jsonify(filtered_products)
+    
+    return jsonify(products)
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
 @app.route("/products/<int:id>")
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    for product in products:
+        if product['id'] == id:
+            return jsonify(product)
+        
+    return jsonify({'message':'Product not found'}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
